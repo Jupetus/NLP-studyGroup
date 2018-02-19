3cbae6bca1eb54621ce30bc1ea0630beabc9b8ad
Pajunen Jukka <pajunej5@false.org.aalto.fi>
2018-02-19 15:09:39 +0200

    Perplexity

--- N-grams/myBaby.py
+++ N-grams/myBaby.py
@@ -1,18 +1,42 @@
 import numpy as np
 import nltk
 from nltk.stem import SnowballStemmer
+from nltk.tokenize import RegexpTokenizer
 from nltk.util import ngrams
 from nltk import word_tokenize
 from collections import Counter
 from nltk.collocations import *
+from operator import itemgetter
 
+"""
+===================
+TODO: Make it so that works for all grams
+- Perplexity
+- Some of the smoothings
+Gosh this came out looking bit confusing
+===================
+"""
 stemmer = SnowballStemmer('english')
+rTokenizer = RegexpTokenizer('\w+')
 
 def stemMyStrings(words):
     newListSAS = []
     for word in words:
         newListSAS.append(stemmer.stem(word))
     return "".join(newListSAS)
+
+def calculate_perplexity(ngrams, n, test_set):
+    prod = 1
+    N = 0
+    for line in test_set:
+        split = line.split()
+        for k in range(n, len(split)):
+            suffix = split[k - 1]
+            word = split[k]
+            likelihood = eval_ngram_word(ngrams, suffix, word)
+            prod *= 1 / likelihood
+            N += 1
+    return abs(prod) ** (1.0 / N)
 
 # Make a n-gram out of list of strings, where each string is a sentance
 # Strings = array of strings
@@ -32,55 +56,102 @@ def make_ngram(strings, n=1):
     return gramDict
 
 # Evaluates given unigram as count(word) / count(*)
-def eval_unigram(ngram, word):
+def eval_unigram(ngram, word, alpha=1.0):
     wordcount = sum(ngram.values())
     if word in ngram.keys():
-        return ngram[word] / wordcount
+        return alpha* ngram[word] / wordcount
     else:
         return 0
 
 # Evaluates given n-gram as: n-gram_count(string) / (n-1)-gram_count(string - 1)
-def eval_gram(ngram, mgram, suffix, word):
+def eval_gram(ngram, mgram, suffix, word, k=0, alpha=1.0, wordcount = 1):
     cond = suffix + ' ' + word
 
     if cond in ngram.keys():
         divident = ngram[cond]
         divisor = mgram[suffix]
-        return divident / divisor
+        return (divident + k) / (divisor + k * wordcount)
     else:
-        return 0
+        return k / wordcount
+
+def kneser_Ney(grams, suffix, word, d=0.5):
+    cond = suffix + ' ' + word
+    divident = 0
+    divisor = grams[1][suffix]
+    if cond in grams[0].keys():
+        divident = max(grams[0][cond] - d, 0)
+
+    norm_constant = d / divisor
+    return divident / divisor + norm_constant * eval_unigram(grams[1], word)
 
 # Evaluates n-gram with given smoothing parameters
 # n-grams: List of n-grams in decreasing order
 # word: word we are evaluating
 # suffix: string trailing the word
-def eval_ngram(ngrams, suffix, word, smoothing = "", k = 1):
+def eval_ngram_word(ngrams, suffix, word, smoothing = "", k = 1, alpha=1.0):
 
-    assert len(ngrams) >= 2, "Atleast two grams yo"
     # add k-smoothing
     if smoothing == "k":
-        return 1
-    # discount smoothing
+        assert len(ngrams) >= 2, "Atleast two grams yo"
+
+        totalWords = sum(ngrams[len(ngrams) - 1].values())
+        return eval_gram(ngrams[0], ngrams[1], suffix, word, k=k, wordcount=totalWords)
+    # discount smoothing got lazy so like kneser-ney
     if smoothing == "d":
-        return 0
+        return kneser_Ney(ngrams, suffix, word)
     # BackOff smoothing
     if smoothing == "bo":
-        return 0
+        if suffix + ' ' + word in ngrams[0].keys():
+            return alpha * eval_gram(ngrams[0], ngrams[1], suffix, word)
+        else:
+            if len(ngrams) > 1:
+                return eval_ngram_word(ngrams[1:], suffix, word, smoothing=smoothing)
+            else:
+                return eval_unigram(ngrams[0], word)
     # Stupid BackOff
     if smoothing == "sbo":
-        return 0
+        if suffix + ' ' + word in ngrams[0].keys():
+            return alpha * eval_gram(ngrams[0], ngrams[1], suffix, word)
+        else:
+            if len(ngrams) > 1:
+                return eval_ngram_word(ngrams[1:], suffix, word, smoothing = smoothing, k = k, alpha=(alpha*0.4))
+            else:
+                return eval_unigram(ngrams[0], word, alpha=alpha)
     # Normal n-gram
     else:
-        return eval_gram(ngrams[0], ngrams[1], suffix, word)
+        assert len(ngrams) >= 2, "Atleast two grams yo"
 
+        return eval_gram(ngrams[0], ngrams[1], suffix, word)
 
 def calculatePMI(ngram2, ngram1, word1, word2):
     if word1 + ' ' + word2 in ngram2.keys():
         divident = eval_gram(ngram2, ngram1, word1, word2)
         divisor = eval_unigram(ngram1, word1) * eval_unigram(ngram1, word2)
-
         return np.log2(divident / divisor)
     return 0
+
+def find_ml_word(gram2, gram1, word):
+    keyList = []
+    for key in gram2.keys():
+        split = key.split()
+        if split[0] == word:
+            keyList.append(key)
+
+    test = [(key.split()[1], eval_ngram_word([gram2, gram1], word, key.split()[1])) for key in keyList]
+    ml = max(test, key=itemgetter(1))[0]
+    ml_word = ml
+
+    return ml_word
+
+def make_a_story(grams, start_word = "i", story_length=100):
+    prevString = start_word
+    story = [start_word]
+    for i in range(story_length):
+        prevString = find_ml_word(grams[0], grams[1], prevString)
+        story.append(prevString)
+
+    return " ".join(story)
+
 
 
 stemmer = SnowballStemmer('english')
@@ -91,24 +162,9 @@ c = Counter()
 with open(path) as f:
     for line in f.readlines():
         review = line.split('$')
-        tokenized = word_tokenize(review[2].lower())
+        tokenized = rTokenizer.tokenize(review[2].lower().strip())
         ngramList.append(stemMyStrings(" ".join(tokenized)))
-
-
 
 bigram = make_ngram(ngramList, 2)
 unigram = make_ngram(ngramList, 1)
 grams = [bigram, unigram]
-
-print(calculatePMI(bigram, unigram, "my", "songs"))
-
-"""
-#test = sorted(bigram.items(), key=lambda x: x[1], reverse=True)
-# print(test)
-# print(bigram["my songs"])
-# print(unigram["my"])
-# print(unigram["songs"])
-# print(calculatePMI(bigram, unigram, "my", "songs"))
-print(len(unigram))
-print(ngramProbability(bigram, unigram, "cat", "my", smoothing="k"))
-"""
